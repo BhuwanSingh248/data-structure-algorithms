27126926efce1b6869483e9f426dcd41b3123c61
bhuwan singh panora <bhuwansingh248@gmail.com>
2024-02-23 01:42:31 +0530

    Time: 41 ms (23.08%) | Memory: 16.5 MB (62.83%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,7 +1,7 @@
 class Solution:
     def __init__(self):
         self.dp = None 
-    def numTrees(self, n: int) -> int:
+    def numTrees1(self, n: int) -> int:
         if not self.dp:
             self.dp = [-1] * (n+1)
         if n <= 1:
@@ -13,3 +13,13 @@ class Solution:
             ans += self.numTrees(i-1) * self.numTrees(n-i)
         self.dp[n] = ans
         return ans 
+    
+    def numTrees(self, n):
+        dp = [0] * (n+1)
+        dp[0],dp[1] = 1, 1
+        #  number of nodes
+        for i in range(2, n+1):
+            # assuing j as root node
+            for j in range(1,n+1):
+                dp[i] += dp[j-1] * dp[i - j]
+        return dp[n]
