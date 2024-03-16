ab5d8b346bbd3cae038771187e7d350e69b4e759
bhuwan singh panora <bhuwansingh248@gmail.com>
2024-03-17 01:09:50 +0530

    Time: 83 ms (5.17%) | Memory: 25 MB (5.70%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        
+        # bruteforce 
+        def helper(i, buy):
+            if i >= len(prices):
+                return 0
+            if buy:
+                profit = max(-prices[i] + helper(i+1, 0), helper(i+1, 1))
+            else:
+                profit = max(prices[i] + helper(i+1, 1), helper(i+1, 0))
+            return profit
+
+        def helperDP(i, buy, dp):
+            if i >= len(prices):
+                return 0
+            if dp[i][buy] != -1:
+                return dp[i][buy]
+            if buy:
+                profit = max(-prices[i] + helperDP(i+1, 0, dp), helperDP(i+1, 1, dp))
+            else:
+                profit = max(prices[i] + helperDP(i+1, 1, dp), helperDP(i+1, 0, dp))
+            
+            dp[i][buy] = profit
+            return profit
+
+        # return helper(0, 1)
+        dp = [[-1] * 2 for _ in prices]
+        ans = helperDP(0, 1,dp)
+        return ans
