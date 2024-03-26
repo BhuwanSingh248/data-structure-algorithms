734660a5c2aebf448cfd4d16e7686a481a1a8705
bhuwan singh panora <bhuwansingh248@gmail.com>
2024-03-27 00:14:38 +0530

    Time: 326 ms (8.85%) | Memory: 30.4 MB (37.59%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -9,9 +9,9 @@ class Solution:
             if abs(nums[i]) <= n and nums[abs(nums[i]) - 1] > 0:
                 nums[abs(nums[i]) - 1] *= -1
 
-        flag = 0        
-        for i in range(n):
-            if nums[i] > 0:
-                flag += 1 
-                return i+1
-        return i+1 if flag > 0 else i+2
+        # flag = 0        
+        for i in range(1, n+1):
+            if nums[i-1] >= 0:
+                # flag += 1 
+                return i
+        return n+1
