ef67bd3ab8c71b48c24e7d36b276eaa0e88eff0b
bhuwan singh panora <bhuwansingh248@gmail.com>
2024-09-09 00:09:31 +0530

    Time: 350 ms (68.12%) | Memory: 17.2 MB (99.88%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -11,4 +11,15 @@ class Solution:
     
         return arr[0] 
 
+    def canJump(self, nums: List[int]) -> bool:
+        curr = 0
+        for i in nums:
+            if curr < 0:
+                return False
+            if i > curr:
+                curr = i
+            curr -= 1
+        return True
+
+
 
