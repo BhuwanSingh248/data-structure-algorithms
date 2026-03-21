d4c2191cea018f68eeeea9f54c08a700710b62d7
bhuwan singh panora <bhuwansingh248@gmail.com>
2026-03-21 14:44:32 +0530

    Time: 3 ms (9.34%) | Memory: 19.4 MB (23.42%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,21 +1,8 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        op = [[1]]
-        def check_bound(r,c,i,j):
-            if j > i:
-                return False
-            if c < 0 or r < 0:
-                return False
-            return True
-            
-        for i in range(1, numRows):
-            temp = [1] * (i + 1)
-            for j in range(i):
-                row = i - 1
-                col = j - 1
-                is_valid = check_bound(row, col, i, j)
-                if is_valid:
-                    temp[j] = op[row][j] + op[row][col]     
-            op.append(temp)
-        return op
-                
+        triangle = [[1] * i for i in range(1, numRows+1)]
+        if numRows > 2:
+            for i in range(2, numRows):
+                for j in range(1, i):
+                    triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+        return triangle
