402ca99e2b849afe34b95d8b9de22a633e3bc619
bhuwan singh panora <bhuwansingh248@gmail.com>
2026-04-09 00:44:57 +0530

    Time: 15 ms (28.89%) | Memory: 21.2 MB (17.51%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,25 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
-        hl, hr = [height[0]], [height[-1]]
-
-        for i in range(1,len(height)):
-            if hl[i-1] < height[i]:
-                hl.append(height[i])
-            else:
-                hl.append(hl[i-1])
+        n = len(height)
+        fe = [0] * n
+        ef = [0] * n
+        prev = height[0]
+        fe[0] = prev
+        ef[-1] = prev
+        for i in range(1, n):
+            prev = max(prev, height[i])
+            fe[i] = prev
+        prev = height[-1]
+        ef[-1] = prev
+        for i in range(n-2, -1, -1):
+            prev = max(prev, height[i])
+            ef[i] = prev
         
-        for i in range(len(height) -2, -1, -1 ):
-            if hr[len(height) - i -2] <  height[i]:
-                hr.append(height[i])
-            else:
-                hr.append(hr[len(height) - i - 2])
-        
-        hr = hr[::-1]
-        trapped = 0
-        for i in range(len(height)):
-            trapped += ((min(hl[i] , hr[i])) - height[i])
-
-        return trapped
+        total = 0
+        for i, (j, k) in enumerate(zip(fe, ef)):
+            total += min(j, k) - height[i]
+        return total
