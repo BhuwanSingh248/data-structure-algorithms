503c811ce403974ed1a6abe212867c0f48eec490
bhuwan singh panora <bhuwansingh248@gmail.com>
2026-04-28 00:48:24 +0530

    Time: 11 ms (47.48%) | Memory: 19.7 MB (34.70%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,22 +2,31 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         candidates.sort()
-        def helper(index, target, temp):
-            if target == 0:
-                res.append(temp)
-                return
+        # def helper(index, target, temp):
+        #     if target == 0:
+        #         res.append(temp)
+        #         return
 
-            if index >= len(candidates):
-                return 
+        #     if index >= len(candidates):
+        #         return 
             
 
-            helper(index + 1, target, temp[:])
+        #     helper(index + 1, target, temp[:])
 
-            new_target = target - candidates[index]
-            if new_target >= 0:
-                temp.append(candidates[index])
-                helper(index, new_target, temp)
+        #     new_target = target - candidates[index]
+        #     if new_target >= 0:
+        #         temp.append(candidates[index])
+        #         helper(index, new_target, temp)
                 
+        def helper(index, target, path):
+            if target == 0: 
+                res.append(path)
+                return
 
+            for i in range(index, len(candidates)):
+                if candidates[index] > target:
+                    break
+                
+                helper(i, target - candidates[i], path + [candidates[i]])
         helper(0, target, [])
         return res
