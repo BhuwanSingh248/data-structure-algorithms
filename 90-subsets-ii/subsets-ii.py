4b1dced99bea498411f66ecc2f21c0d015d0b345
bhuwan singh panora <bhuwansingh248@gmail.com>
2026-04-27 23:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (60.92%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,13 +2,14 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         op = [[]]
-        last_size = 0
-        for i in range(len(nums)):
-            start = last_size if i > 0 and nums[i] == nums[i-1] else 0
-            current_size = len(op)
+        last_index = 0
 
+        for i in range(len(nums)):
+            start = last_index if i > 0 and nums[i] == nums[i-1] else 0
+            current_size = len(op)
             for j in range(start, current_size):
                 op.append(op[j] + [nums[i]])
             
-            last_size = current_size
+            last_index = current_size
+        
         return op
